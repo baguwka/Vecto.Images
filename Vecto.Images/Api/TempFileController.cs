8e8f78f7a95340b1b620693f0a48cba895445115
akop <ttehbb@gmail.com>
2022-03-12 12:57:04 +0300

    hotfix*

---
+++ Vecto.Images/Api/TempFileController.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Vecto.Images.Contract;
+
+namespace Vecto.Images.Api
+{
+    [Microsoft.AspNetCore.Components.Route("/api/v1/temp-files")]
+    public class TempFileController
+    {
+        [HttpPost]
+        public async Task<FilePrimitive> Upload(IFormFile file)
+        {
+            
+        }
+    }
+    
+}
